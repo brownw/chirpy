ecb770b09d20a170e2c61665e1392fb9af42c27d
Bill Brown <brownw@gmail.com>
2026-05-11 22:09:29 -0400

    add readiness handler and update health check route

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import "net/http"
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", readinessHandler)
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
